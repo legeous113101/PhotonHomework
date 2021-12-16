19fc9a9e2f29ea245cf712c67131b2006b611e20
Yong <legeous113101@gmail.com>
2021-12-16 18:19:27 +0800

    Fixed Create room bug

--- PhotonHomework/Assets/MyScripts/Core/GameManager.cs
+++ PhotonHomework/Assets/MyScripts/Core/GameManager.cs
@@ -1,57 +1,45 @@
 using Photon.Pun;
 using Photon.Realtime;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-namespace MyScripts.Core
+namespace Tanks
 {
     public class GameManager : MonoBehaviourPunCallbacks
     {
         public static GameManager instance;
+
         string gameVersion = "1";
-        public GameObject localPlayer;
 
         void Awake()
         {
             if (instance != null)
             {
-                Debug.LogErrorFormat(gameObject,
-                        "Multiple instances of {0} is not allow", GetType().Name);
+                Debug.LogErrorFormat(gameObject, "Multiple instances of {0} is not allow", GetType().Name);
                 DestroyImmediate(gameObject);
                 return;
             }
+
             PhotonNetwork.AutomaticallySyncScene = true;
             DontDestroyOnLoad(gameObject);
             instance = this;
-
         }
-        private void Start()
+
+        void Start()
         {
             PhotonNetwork.ConnectUsingSettings();
             PhotonNetwork.GameVersion = gameVersion;
-            SceneManager.sceneLoaded += OnSceneLoad;
-        }
-
-        private void OnSceneLoad(Scene scene, LoadSceneMode mode)
-        {
-            if (!PhotonNetwork.InRoom)
-            {
-                return;
-            }
-            localPlayer = PhotonNetwork.Instantiate("Tankplayer", new Vector3(0, 0, 0), Quaternion.identity, 0);
         }
 
         public override void OnConnected()
         {
             Debug.Log("PUN Connected");
         }
+
         public override void OnConnectedToMaster()
         {
             Debug.Log("PUN Connected to Master");
         }
+
         public override void OnDisconnected(DisconnectCause cause)
         {
             Debug.LogWarningFormat("PUN Disconnected was called by PUN with reason {0}", cause);
@@ -63,19 +51,21 @@ namespace MyScripts.Core
             {
                 MaxPlayers = 6
             };
+
             PhotonNetwork.JoinOrCreateRoom("Kingdom", options, null);
         }
 
         public override void OnJoinedRoom()
         {
+            var ismaster = PhotonNetwork.IsMasterClient;
             if (PhotonNetwork.IsMasterClient)
             {
-                print("Create room.");
+                Debug.Log("Created room!!");
                 PhotonNetwork.LoadLevel("GameScene");
             }
             else
             {
-                print("Join Room!");
+                Debug.Log("Joined room!!");
             }
         }
     }
